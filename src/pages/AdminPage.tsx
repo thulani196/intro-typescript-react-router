6d2189995e3a33dee47cef9dc5f1d5d52dd741de
thulani196 <tembothulani96@outlook.com>
2021-01-20 00:26:00 +0200

    enabled Lazy loading on AdminPage to improve app start up time

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { NavLink, Route, RouteComponentProps } from "react-router-dom";
 import AdminProducts from "./AdminProducts";
 import AdminUsers from "./AdminUsers";
--- src/utils/routes/Routes.tsx
+++ src/utils/routes/Routes.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -6,11 +6,11 @@ import {
   Switch,
 } from "react-router-dom";
 import Header from "../../components/Header";
-import AdminPage from "../../pages/AdminPage";
 import LoginPage from "../../pages/authPages/LoginPage";
 import NotFoundPage from "../../pages/NotFoundPage";
 import ProductPage from "../../pages/ProductPage";
 import ProductsPage from "../../pages/ProductsPage";
+const AdminPage = React.lazy(() => import("../../pages/AdminPage"));
 
 const Routes = () => {
   const [loggedIn, setLoggedIn] = React.useState(true);
@@ -21,7 +21,15 @@ const Routes = () => {
         <Redirect exact={true} from="/" to="/products" />
         <Route path="/login" component={LoginPage} />
         <Route path="/admin">
-          {loggedIn ? <AdminPage /> : <Redirect to="/login" />}
+          {loggedIn ? (
+            <Suspense
+              fallback={<div className="pagecontainer">Loading...</div>}
+            >
+              <AdminPage />
+            </Suspense>
+          ) : (
+            <Redirect to="/login" />
+          )}
         </Route>
         <Route path="/products" exact={true} component={ProductsPage} />
         <Route path="/products/:id" component={ProductPage} />
